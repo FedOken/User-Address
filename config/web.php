fa9f6fa88fde8308e571a48b4d9466344aa4731e
Alexander <agoodminute@gmail.com>
2019-11-15 15:39:40 +0200

    Base pretty url setting and db connection

--- config/web.php
+++ config/web.php
@@ -7,12 +7,14 @@ $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
+    'defaultRoute' => 'site/index',
     'aliases' => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
     'components' => [
         'request' => [
+            'baseUrl'=> '',
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'QzGLQxIcv7vdP6dLae1p39I_WaBvYVsU',
         ],
@@ -43,14 +45,14 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
+            //'enableStrictParsing' => true,
             'showScriptName' => false,
             'rules' => [
+                '<controller>/<action>' => '<controller>/<action>'
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
